cd1bd1b80e1d51bf683c0a6c5394e762082568f2
Lucasluandersonsilva <https://github.com/Lucasluandersonsilva>
2024-08-20 19:57:21 -0300

    inclusão do projeto

---
+++ src/test/java/regressivos/ui/bank/elementos/ElementosWeb.java
@@ -0,0 +1,10 @@
+package regressivos.ui.bank.elementos;
+
+import org.openqa.selenium.By;
+
+public class ElementosWeb {
+	
+	By btnLogin = By.xpath("//button[@class='login-button']");
+	By username = By.xpath("//input[@name='username']");
+
+}
